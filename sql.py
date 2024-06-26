e1f26333fc60e9cb89b11a12aa748257c3dcb151
Beny <b4123190@gmail.com>
2024-06-26 14:46:56 +0300

    new class for connection

--- sql.py
+++ sql.py
@@ -1,89 +1,68 @@
-import pyodbc
+# main.py (או כל שם קובץ אחר)
 
-server = 'DESKTOP-QCOJ24N\\SQLEXPRESS'
-database = 'matchmaking'
-driver = '{ODBC Driver 17 for SQL Server}'
+from sql_key import SQL
 
+# יצירת אובייקט מהמחלקה SQL
+sql = SQL()
 
-connection = pyodbc.connect(
-    f'DRIVER={driver};SERVER={server};DATABASE={database};Trusted_Connection=yes;')
+# דוגמאות לשימוש במתודות המחלקה
+try:
+    exe1 = "DELETE FROM Male WHERE age = 34"
+    show = "insert into Male(id,f_name,l_name,addres,age) values(10,'daniel','biton','22 sireni st',31)"
+    sql.execute(show)
+    sql.commit()
 
-cursor = connection.cursor()
+    s = "select * from Male"
+    sql.execute(s)
+    for row in sql.fetchall():
+        print(row)
 
-
-exe1 = "DELETE FROM Male WHERE age = 34"
-cursor.execute(exe1)
-
-
-connection.commit()
-
-
-show = "insert into Male(id,f_name,l_name,addres,age) values(10,'daniel','biton','22 sireni st',31)"
-cursor.execute(show)
-connection.commit()
-
-s = "select * from Male"
-cursor.execute(s)
-for row in cursor:
-    print(row)
-
-
-update_query = """
+    update_query = """
         UPDATE Male
         SET age = DATEDIFF(year, DateOfBirth, GETDATE())
         WHERE DateOfBirth IS NOT NULL
-        """
-cursor.execute(update_query)   
-connection.commit()     
+    """
+    sql.execute(update_query)
+    sql.commit()
 
-cursor.close()
-connection.close()
+    query_male = "SELECT * from ProfiLeMale"
+    query_female = "SELECT * FROM ProfileFemale"
 
+    sql.execute(query_male)
+    males = sql.fetchall()
 
-query_male = "SELECT * from ProfiLeMale"
-query_female = "SELECT * FROM ProfileFemale"
+    sql.execute(query_female)
+    females = sql.fetchall()
 
-cursor.execute(query_male)
-males = cursor.fetchall()
+    matches = []
 
-cursor.execute(query_female)
-females = cursor.fetchall()
+    for male in males:
+        (serial_number, male_first_name, male_last_name, father_name, mother_name, male_city, israel_abroad, male_age,
+         studying_working, sect, skin_color, height, eye_color, weight, date_of_birth) = male
 
+        for female in females:
+            (serial_number, female_first_name, female_last_name, father_name, mother_name, female_city, israel_abroad, female_age,
+             studying_working, sect, skin_color, height, eye_color, weight, date_of_birth) = female
 
-matches = []
-
-for male in males:
-     (serial_number,male_first_name,male_last_name,father_name,mother_name,male_city,israel_abroad,male_age,
-     studying_working,sect,skin_color,height,eye_color,weight,date_of_birth) = male
-
-     for female in females:
-         
-         (serial_number,female_first_name,female_last_name,father_name,mother_name,female_city,israel_abroad,female_age, 
-         studying_working,sect,skin_color,height,eye_color,weight,date_of_birth) = female
-        
-         if abs(male_age - female_age) <= 3:
-            
-            if male_city == female_city:
-                
+            if abs(male_age - female_age) <= 3 and male_city == female_city:
                 matches.append((male_first_name, male_last_name, female_first_name, female_last_name))
 
+    for match in matches:
+        print(f"Match found: {match[0]} {match[1]} and {match[2]} {match[3]}")
 
-for match in matches:
-    print(f"Match found: {match[0]} {match[1]} and {match[2]} {match[3]}")
-    
-connection.commit() 
-print("ProfileMale data after insert:")
-cursor.execute("SELECT * FROM ProfileMale")
-males_after_insert = cursor.fetchall()
-for male in males_after_insert:
-    print(male)
+    sql.commit()
+    print("ProfileMale data after insert:")
+    sql.execute("SELECT * FROM ProfileMale")
+    males_after_insert = sql.fetchall()
+    for male in males_after_insert:
+        print(male)
 
-print("ProfileFemale data after insert:")
-cursor.execute("SELECT * FROM ProfileFemale")
-females_after_insert = cursor.fetchall()
-for female in females_after_insert:
-    print(female)
-cursor.close()
-connection.close()   
+    print("ProfileFemale data after insert:")
+    sql.execute("SELECT * FROM ProfileFemale")
+    females_after_insert = sql.fetchall()
+    for female in females_after_insert:
+        print(female)
 
+finally:
+    sql.close()
 
